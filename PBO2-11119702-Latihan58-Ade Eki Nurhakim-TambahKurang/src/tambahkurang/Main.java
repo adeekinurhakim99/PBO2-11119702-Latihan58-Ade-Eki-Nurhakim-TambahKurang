c913080509122872917b4d1deebfcddd06fd2437
Ade Ekii Nurhakim <56956036+adeekinurhakim99@users.noreply.github.com>
2019-11-02 20:58:37 +0700

    Add files via upload

---
+++ PBO2-11119702-Latihan58-Ade Eki Nurhakim-TambahKurang/src/tambahkurang/Main.java
@@ -0,0 +1,23 @@
+/*Nama 	 : Ade Eki Nurhakim
+NIM 	 : 10119702
+Fakultas : Teknik Dan Ilmu Komputer
+Prodi 	 : Teknik Informatika
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tambahkurang;
+
+/**
+ *
+ * @author Ekiw
+ */
+public class Main {
+    public static void main(String[] args) {
+    jumlahBilangan jbil = new jumlahBilangan();
+    jbil.tampilHasilJumlah();
+    
+    SelisihBilangan sbil = new SelisihBilangan();
+    sbil.tampilSelisih();
+    }
+    } 
---
+++ PBO2-11119702-Latihan58-Ade Eki Nurhakim-TambahKurang/src/tambahkurang/SelisihBilangan.java
@@ -0,0 +1,19 @@
+/*Nama 	 : Ade Eki Nurhakim
+NIM 	 : 10119702
+Fakultas : Teknik Dan Ilmu Komputer
+Prodi 	 : Teknik Informatika
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tambahkurang;
+
+/**
+ *
+ * @author Ekiw
+ */
+public class SelisihBilangan extends Bilangan {
+    public void tampilSelisih(){
+    System.out.println("Hasil Selisih : ");
+    }
+}
